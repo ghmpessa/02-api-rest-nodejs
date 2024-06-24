9999ed14ab52f60962b895af1fd0d00ba07cb81b
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-23 22:17:49 -0300

    chore: add useNullAsDefault to knex setup to remove warning

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,7 @@ import { knex as setupKnex } from 'knex'
 export const knex = setupKnex({
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db'
-  }
+    filename: './tmp/app.db',
+  },
+  useNullAsDefault: true,
 })
