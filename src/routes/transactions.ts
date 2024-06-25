8ded2644fd8fe1f317a4d20cccb8f608e5914736
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-24 21:57:29 -0300

    feat: add find transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -12,6 +12,20 @@ export async function transactionsRoutes(app: FastifyInstance) {
     }
   })
 
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').select().where('id', id).first()
+
+    return {
+      transaction
+    }
+  })
+
   app.post('/', async (request, response) => {
 
     const createTransactionBodySchema = z.object({
