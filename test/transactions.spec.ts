ffdbc2a12264f56b1a2b5cecb781294cdd8ae750
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-25 20:03:13 -0300

    test: add database migration calls before each test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,8 @@
-import { test, beforeAll, afterAll, describe, expect } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
+import { afterEach } from 'node:test'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -9,6 +11,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   test('User should be able to create a new transaction', async () => {
