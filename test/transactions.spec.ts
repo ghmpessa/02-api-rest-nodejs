218e40febec710241806993c4894dc75787b221f
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-25 20:33:43 -0300

    test: ensure /transactions/summary route returns summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -71,7 +71,6 @@ describe('Transactions routes', () => {
     const transactionListResponse = await request(app.server)
       .get('/transactions')
       .set('Cookie', cookies!)
-      .expect(200)
 
     const transactionId = transactionListResponse.body.transactions[0].id
 
@@ -85,6 +84,37 @@ describe('Transactions routes', () => {
         amount: 1800,
       })
     )
+  })
+
+  test('User should be able to get summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 1800,
+        type: 'credit'
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    expect(cookies).not.toBeNull()
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies!)
+      .send({
+        title: 'Debit transaction',
+        amount: 900,
+        type: 'debit'
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies!)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 900,
+    })
   })
 
 })
